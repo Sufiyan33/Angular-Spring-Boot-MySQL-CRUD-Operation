21a1c8edffaaebe8afa8a7f43bca92ddc102afaa
Sufiyan33 <basu700736@gmail.com>
2024-11-15 15:16:46 +0530

    Removed lomboc and added getter and setter

--- crud operation/crud-operation/src/main/java/com/sufiyan/entity/Customer.java
+++ crud operation/crud-operation/src/main/java/com/sufiyan/entity/Customer.java
@@ -5,9 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "Customer")
 public class Customer {
@@ -19,4 +17,59 @@ public class Customer {
 	private String lastName;
 	private String email;
 	private String phone;
+	
+	public Customer(Long id, String firstName, String lastName, String email, String phone) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phone=" + phone + "]";
+	}
 }
