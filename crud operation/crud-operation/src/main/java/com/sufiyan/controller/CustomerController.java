0ad7c3b93a6d6d9f03b3a02a531ef8e9a0b13181
Sufiyan33 <basu700736@gmail.com>
2024-11-15 15:16:16 +0530

    Adding logic to save customer in db

--- crud operation/crud-operation/src/main/java/com/sufiyan/controller/CustomerController.java
+++ crud operation/crud-operation/src/main/java/com/sufiyan/controller/CustomerController.java
@@ -1,8 +1,23 @@
 package com.sufiyan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sufiyan.entity.Customer;
+import com.sufiyan.service.CustomerService;
+
 @RestController
+@RequestMapping("/api")
 public class CustomerController {
 
+	@Autowired
+	private CustomerService service;
+	
+	@PostMapping("/customer")
+	public Customer postCustomer(@RequestBody Customer customer) {
+		return service.postCustomer(customer);
+	}
 }
--- crud operation/crud-operation/src/main/java/com/sufiyan/service/CustomerService.java
+++ crud operation/crud-operation/src/main/java/com/sufiyan/service/CustomerService.java
@@ -1,8 +1,17 @@
 package com.sufiyan.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.sufiyan.entity.Customer;
+import com.sufiyan.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
 
+	@Autowired
+	private CustomerRepository customerRepo;
+	
+	public Customer postCustomer(Customer customer) {
+		return customerRepo.save(customer);
+	}
 }
