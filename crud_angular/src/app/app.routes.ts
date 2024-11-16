58bf8283406ceac6fe88cea259b82022af3c85c8
Sufiyan33 <basu700736@gmail.com>
2024-11-16 15:55:25 +0530

    Added routing to get all data

--- crud_angular/src/app/app.routes.ts
+++ crud_angular/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { PostCustomerComponent } from './components/post-customer/post-customer.component';
+import { GetAllCustomerComponent } from './components/get-all-customer/get-all-customer.component';
 
 export const routes: Routes = [
     {
         path: 'customer',
         component: PostCustomerComponent
+    },
+    {
+        path: '',
+        component: GetAllCustomerComponent
     }
 ];
