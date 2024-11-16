cdce56dafc887f94542af71b6e64d900cce1e171
Sufiyan33 <basu700736@gmail.com>
2024-11-16 17:22:06 +0530

    Fetching customer by id and updating data

--- crud_angular/src/app/components/update-customer/update-customer.component.ts
+++ crud_angular/src/app/components/update-customer/update-customer.component.ts
@@ -1,29 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from '../../services/customer.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-customer',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './update-customer.component.html',
   styleUrl: './update-customer.component.css'
 })
 export class UpdateCustomerComponent implements OnInit{
+  
+  updateCustomerForm: FormGroup= new FormGroup({});
 
   id!: number;
   constructor(private route: ActivatedRoute, 
-    private service: CustomerService){
+    private service: CustomerService, private fb: FormBuilder){
       this.id = this.route.snapshot.params['id'];
     }
 
  
   ngOnInit(): void {
+    this.updateCustomerForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [null, Validators.required, Validators.email],
+      phone: [null, Validators.required]
+    })
+
     this.getCustomerUsingId();
   }
   getCustomerUsingId(){
     this.service.getCustomerById(this.id).subscribe((res) =>{
       console.log(res);
+      this.updateCustomerForm.patchValue(res);
     })
   }
 }
