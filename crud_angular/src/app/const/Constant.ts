ff8e236e30aeea8230295127a19ad8c87faba9d3
Sufiyan33 <basu700736@gmail.com>
2024-11-18 11:30:05 +0530

    Created constant file for validation message

--- crud_angular/src/app/const/Constant.ts
+++ crud_angular/src/app/const/Constant.ts
@@ -1,3 +1,11 @@
 export const Constant = {
+
+    VALIDATING_MESSAGE:{
+        FIRST_NAME_REQUIRED: 'firstName required',
+        LAST_NAME_REQUIRED: 'lastName required',
+        EMAIL_REQUIRED: 'email required',
+        PHONE_REQUIRED: 'phone required'
+    },
+
     BASIC_URL: 'http://localhost:9000/api'
 }
